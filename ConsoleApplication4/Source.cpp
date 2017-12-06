38f1338e8ab1272a0896480bdbdb015198afe99d
СеитБ <@227_5>
2017-12-06 09:43:29 +0600

    Add project files.

---
+++ ConsoleApplication4/Source.cpp
@@ -0,0 +1,39 @@
+#include <stdint.h>
+#include <iostream>
+#include <math.h>
+#include <locale.h>
+
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+
+start:
+	cout << "введите номер задания = ";
+	int NZ;
+	cin >> NZ;
+	if (NZ == 1)
+	{
+		//операторы switch
+		int x;
+		switch (x) //if
+		{
+		case 6: //else
+		{
+			cout << "x=6";
+		}
+		break;
+		
+		default: //else if
+		{
+
+		}
+		break;
+		
+		}
+		
+
+	}
+
+	
+}
